c827632c80752c61a7c031bc80ec2de8d0cd353c
Armin <armin.ag@outlook.com>
2017-01-26 15:58:55 +0100

    Add Control Initialization and Bind Events

--- app/src/main/java/solutions/hedron/android_workout_app/MainActivity.java
+++ app/src/main/java/solutions/hedron/android_workout_app/MainActivity.java
@@ -2,6 +2,8 @@ package solutions.hedron.android_workout_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout weightBtn = (RelativeLayout)findViewById(R.id.weightBtn);
+        RelativeLayout yogaBtn = (RelativeLayout)findViewById(R.id.yogaBtn);
+        RelativeLayout cardioBtn = (RelativeLayout)findViewById(R.id.cardioBtn);
+
+        weightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Load Details Activity
+            }
+        });
+
+        yogaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Load Details Activity
+            }
+        });
+
+        cardioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Load Details Activity
+            }
+        });
     }
 }
